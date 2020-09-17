b00380b9e4667e11b373be5f3572e8d9e5381d2b
root <root@vra-000844.cpod-vrealizesuite.az-demo.shwrfr.com>
2020-09-17 09:33:53 +0200

    transformer release

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,12 +1,3 @@
-#variable "public_key_path" {
-#  description = <<DESCRIPTION
-#Path to the SSH public key to be used for authentication.
-#Ensure this keypair is added to your local SSH agent so provisioners can
-#connect.
-
-#Example: ~/.ssh/terraform.pub
-#DESCRIPTION
-#}
 variable "instance_number" {
 	description = "enter the number of instances needed"
 }
@@ -15,6 +6,4 @@ variable "instance_number" {
 variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "eu-west-1"
-}
-
 }
